e3be2add24eb0ba35d1bbe9cc33bf1fb1ed85837
Meagan Mason <meagananne@Meagans-MacBook-Pro.local>
2016-11-19 14:57:14 -0800

    got if/else statement working for author fadeIn

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -15,19 +15,22 @@ articleView.populateFilters = function() {
     }
   });
 };
-articleView.populateFilters();
+
 
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
-      $('').hide();
-  //     /* TODO: If the slect box changes to an option that has a value, we should:
+      $('article').hide();
+      $(`article[data-author="${ $(this).val() }"]`).fadeIn(1000)
+      // $('article[data-author=" ' + $(this).val() + '"]').show();
+  //     /* TODO: DONE: If the slect box changes to an option that has a value, we should:
   //         1. Hide all of the articles
   //         2. Fade in only the articles that match based on on the author
   //           that was aselected. Hint: use an attribute selector to find
   //           those articles that match the value, and then fade them in.
   //       */
     } else {
+      $('article').show();
     /* Otherwise, we should:
         1. Show all the articles except the template */
     }
@@ -65,3 +68,5 @@ articleView.setTeasers = function() {
 };
 
 // TODO: Invoke all of the above functions (I mean, methods!):
+articleView.populateFilters();
+articleView.handleAuthorFilter();
