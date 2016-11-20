089682d75587f81746914f3e01e0048b21ad0e86
Meagan Mason <meagananne@Meagans-MacBook-Pro.local>
2016-11-19 16:25:18 -0800

    fixing merge issue

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -22,16 +22,15 @@ articleView.handleAuthorFilter = function() {
     if ($(this).val()) {
       $('article').hide();
       $(`article[data-author="${ $(this).val() }"]`).fadeIn(1000)
-  //     /* TODO: DONE: If the slect box changes to an option that has a value, we should:
-  //         1. Hide all of the articles
-  //         2. Fade in only the articles that match based on on the author
-  //           that was aselected. Hint: use an attribute selector to find
-  //           those articles that match the value, and then fade them in.
-  //       */
+      // $('article[data-author=" ' + $(this).val() + '"]').show();
+      //     /* TODO: DONE: If the slect box changes to an option that has a value, we should:
+      //         1. Hide all of the articles
+      //         2. Fade in only the articles that match based on on the author
+      //           that was aselected. Hint: use an attribute selector to find
+      //           those articles that match the value, and then fade them in.
+      //       */
     } else {
       $('article').not('.template').show();
-    /* Otherwise, we should:
-        1. Show all the articles except the template */
     }
     $('#category-filter').val('');
   });
@@ -58,28 +57,29 @@ articleView.handleCategoryFilter = function() {
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
     console.log($(this).data('content'));
-    if ($(this).data('content')) {
+  if ($(this).data('content')) {
     $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn(1000);
+     $('#' + $(this).data('content')).fadeIn(1000);
   };
 })
-    /* TODO:
-      1. Hide all of the .tab-content sections
-      2. Fade in the single .tab-content section that is
-        associated with the .tab element's data-content attribute.
-    */
+  // /* TODO: DONE:
+  //   1. Hide all of the .tab-content sections
+  //   2. Fade in the single .tab-content section that is
+  //     associated with the .tab element's data-content attribute.
+  // */
+  // });
   $('.main-nav .tab:first').click();
 };
 
 articleView.setTeasers = function() {
   $('.article-body *:nth-of-type(n+2)').hide();
   /* TODO: Add a delegated event handler to reveal the remaining paragraphs.
-    When a .read-on link is clicked, we can:
-    1. Prevent the defaul actionof a link.
-    2. Reveal everything in that particular article now.
-    3. Hide that read-on link!
+  When a .read-on link is clicked, we can:
+  1. Prevent the defaul actionof a link.
+  2. Reveal everything in that particular article now.
+  3. Hide that read-on link!
 
-    // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
+  // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
   */
 };
 
