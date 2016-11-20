48cc02e86a7933672dbaa417280f8d0d6b818d56
olds1423 <olds1423@gmail.com>
2016-11-19 16:10:28 -0800

    completed handleMainNav with the exptertise of mr scott

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -23,12 +23,12 @@ articleView.handleAuthorFilter = function() {
       $('article').hide();
       $(`article[data-author="${ $(this).val() }"]`).fadeIn(1000)
       // $('article[data-author=" ' + $(this).val() + '"]').show();
-  //     /* TODO: DONE: If the slect box changes to an option that has a value, we should:
-  //         1. Hide all of the articles
-  //         2. Fade in only the articles that match based on on the author
-  //           that was aselected. Hint: use an attribute selector to find
-  //           those articles that match the value, and then fade them in.
-  //       */
+      //     /* TODO: DONE: If the slect box changes to an option that has a value, we should:
+      //         1. Hide all of the articles
+      //         2. Fade in only the articles that match based on on the author
+      //           that was aselected. Hint: use an attribute selector to find
+      //           those articles that match the value, and then fade them in.
+      //       */
     } else {
       $('article').not('.template').show();
     }
@@ -56,24 +56,30 @@ articleView.handleCategoryFilter = function() {
 
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
-    /* TODO:
-      1. Hide all of the .tab-content sections
-      2. Fade in the single .tab-content section that is
-        associated with the .tab element's data-content attribute.
-    */
-  });
+    console.log($(this).data('content'));
+  if ($(this).data('content')) {
+    $('.tab-content').hide();
+     $('#' + $(this).data('content')).fadeIn(1000);
+  };
+})
+  // /* TODO: DONE:
+  //   1. Hide all of the .tab-content sections
+  //   2. Fade in the single .tab-content section that is
+  //     associated with the .tab element's data-content attribute.
+  // */
+  // });
   $('.main-nav .tab:first').click();
 };
 
 articleView.setTeasers = function() {
   $('.article-body *:nth-of-type(n+2)').hide();
   /* TODO: Add a delegated event handler to reveal the remaining paragraphs.
-    When a .read-on link is clicked, we can:
-    1. Prevent the defaul actionof a link.
-    2. Reveal everything in that particular article now.
-    3. Hide that read-on link!
+  When a .read-on link is clicked, we can:
+  1. Prevent the defaul actionof a link.
+  2. Reveal everything in that particular article now.
+  3. Hide that read-on link!
 
-    // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
+  // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
   */
 };
 
@@ -81,3 +87,4 @@ articleView.setTeasers = function() {
 articleView.populateFilters();
 articleView.handleAuthorFilter();
 articleView.handleCategoryFilter();
+articleView.handleMainNav();
